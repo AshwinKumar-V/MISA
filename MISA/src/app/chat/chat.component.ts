5ac602808b70a1ad86ef4e5a6fa8ef3f820f239b
AshwinKumar-V <ashwingt20001@gmail.com>
2023-03-18 00:57:13 +0530

    integrated client to NLPService

--- MISA/src/app/chat/chat.component.ts
+++ MISA/src/app/chat/chat.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-chat',
@@ -13,13 +15,31 @@ export class ChatComponent implements OnInit {
     text: string
   }> = [];
 
+  constructor(
+    private http: HttpClient) {}
+
   ngOnInit(): void {
     this.history.splice(0, this.history.length)
   }
 
-  sendMessage() {
+  async sendMessage() {
     this.history.push({role: "user", text: this.message})
-    this.history.push({role: "assistant", text: this.message})
     this.message = ''
+
+    // get response from NLP service
+    try{
+      var data = await lastValueFrom(this.http.post("http://127.0.0.1:3000/ask", {prompt: this.message}))
+      if ((data as any).success) {
+        this.history.push({role: "assistant", text: (data as any).message})
+      }
+      else {
+        console.error("Server error.")
+      }
+    }
+    catch(err) {
+      console.error("Server error.")
+    }
   }
+
+
 }
