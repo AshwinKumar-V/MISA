e17c50c32ff3858b579d7265d2c0e7cd90e7fe61
Sain.Saji <Sain.Saji@trivium-esolutions.com>
2023-04-27 11:21:43 +0530

    updated sidebar,panel and user elements

--- FrontEnd/MISA/src/app/app-routing.module.ts
+++ FrontEnd/MISA/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { UserComponent } from './user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component:UserComponent,
+    path:'user'
+  },
+  {
+    component:AdminComponent,
+    path:'admin'
+  },
+  {
+    path:"**",
+    redirectTo:"user"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
